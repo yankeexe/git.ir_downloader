0281d0670558844865706035c1d91b36dc504261
Yankee <yankee.exe@gmail.com>
2020-04-10 19:44:28 +0545

    Add argparse

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import re
 import os
 import requests
+import argparse
+
 from bs4 import BeautifulSoup
 
 from constants import PACKAGE_MANAGERS
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(description='Process git.ir links.')
+
+    parser.add_argument('link', type = str, help = "git.ir URL")
+    parser.add_argument('-n', '--name', help = "Folder name to store the downloaded files")
+    parser.add_argument('-e', '--extract', help = "Used in case of a zipped file", default = True)
+
+    return parser.parse_args()
 
 
 def identify_system():
@@ -17,10 +29,11 @@ def identify_system():
             return name
 
 
-def parse_url(url):
+def parse_url(args):
     """
     Prase url and extract links
     """
+    url = args.link
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -38,23 +51,28 @@ def parse_url(url):
     return folder_title
 
 
-def download_files(folder_title):
+def download_files(folder_title, args = ''):
     """
     Download files when the given URL is parsed
     """
+    if args:
+        name = args.name
+    name = folder_title
+
     os.system('echo Creating folder')
-    os.system(f'mkdir {folder_title}')
+    os.system(f'mkdir {name}')
     os.system('echo Initiating download...')
-    os.system(f"cd {folder_title} && wget -i ../.links.txt")
+    os.system(f"cd {name} && wget -i ../.links.txt")
 
 
 def start():
     """
     entrypoint for the app
     """
-    url = input("Input git.ir URL: ")
-    folder_title = parse_url(url)
-    download_files(folder_title)
+    args = init_argparse()
+
+    folder_title = parse_url(args)
+    download_files(folder_title, args)
 
 
 start()
