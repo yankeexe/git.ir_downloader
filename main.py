938768e78adcd2c68471bdb8bd683efd19556d15
Yankee <yankee.exe@gmail.com>
2020-04-12 15:46:38 +0545

    Import modules, handle keyboard interrupt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import argparse
 
-from gitir_downloader.constants import PACKAGE_MANAGERS
-from gitir_downloader.downloader import parse_url, download_files
+from gitir_downloader.parser import parse_url
+from gitir_downloader.downloader import download_files
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,34 +13,35 @@ def init_argparse():
     """
     Initialize argparse module for commandline argument parsing.
     """
-    parser = argparse.ArgumentParser(description='Process git.ir links.', epilog = "Enjoy the program :)")
+    parser = argparse.ArgumentParser(
+        description="Downnload video files from git.ir links.",
+        epilog="Enjoy the program :)",
+    )
 
-    parser.add_argument('link', type = str, help = "git.ir URL")
-    parser.add_argument('-n', '--name', help = "Folder name to store the downloaded files")
-    parser.add_argument('-e', '--extract', help = "Used in case of a zipped file", default = True)
+    parser.add_argument("link", type=str, help="git.ir URL")
+    parser.add_argument(
+        "-n", "--name", help="Folder name to store the downloaded files"
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
     return parser.parse_args()
-
-
-def identify_system():
-    """
-    Identify the package manager for the system.
-    """
-    for name in PACKAGE_MANAGERS:
-        console_out = os.popen(f'which {name}').read()
-
-        if '/usr/bin' in console_out:
-            return name
 
 
 def start():
     """
     entrypoint for the app
     """
-    args = init_argparse()
+    try:
+        args = init_argparse()
 
-    folder_title = parse_url(args)
-    download_files(folder_title, args)
-
-
-start()
+        folder_title, LINKS = parse_url(args)
+        download_files(folder_title, LINKS, args)
+    except KeyboardInterrupt:
+        print("Stopped Downloading!" + " \N{cross mark}")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
