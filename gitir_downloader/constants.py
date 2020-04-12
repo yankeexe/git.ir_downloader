26bb839f5885fe8bad7dcf166e07606f5bb3af4b
Yankee <yankee.exe@gmail.com>
2020-04-12 19:39:45 +0545

    deleted file

--- gitir_downloader/constants.py
+++
@@ -1,7 +0,0 @@
-""" Constants for the script. """
-from pathlib import Path
-
-PACKAGE_MANAGERS = ['whatever', 'pacman', 'apt']
-
-# ROOT_PATH = Path(__file__).parent / "../"
-ROOT_PATH = Path(__file__).resolve().parents[1]
