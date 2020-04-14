1fef1294b514df11baaf52c9d0a012862985cf0e
yankeexe <yankee.exe@gmail.com>
2020-04-14 14:14:41 +0545

    Remove unnecessary packages

--- setup.py
+++ setup.py
@@ -7,56 +7,27 @@ with open("README.md", "r") as f:
 
 requirements = [
     "beautifulsoup4",
-    "bleach",
-    "certifi",
-    "cffi",
-    "chardet",
-    "colorama",
-    "cryptography",
-    "cursor",
-    "docutils",
     "halo",
-    "idna",
-    "importlib-metadata",
-    "jeepney",
-    "keyring",
-    "log-symbols",
-    "pkginfo",
-    "pycparser",
-    "Pygments",
-    "readme-renderer",
     "requests",
-    "requests-toolbelt",
-    "SecretStorage",
-    "six",
-    "soupsieve",
-    "spinners",
-    "termcolor",
-    "tqdm",
-    "twine",
-    "urllib3",
-    "webencodings",
-    "zipp"
+    "tqdm"
 ]
 
 # Development Requirements
 requirements_dev = ["pytest<=4.*", "black<=19.10b0"]
 
 setuptools.setup(
-    name="gitir_downloader",
-    version="0.0.6",
+    name="gitir_download",
+    version="0.0.5",
     author="Yankee Maharjan",
     author_email="yankee.exe@gmail.com",
     description="Download videos from scattered links of git.ir",
-    url="https://github.com/yankeexe/gitir-downloader",
     license="MIT",
-    packages=setuptools.find_packages("git.ir_downloader"),
-    package_dir={"": "gitir_downloader"},
+    packages = setuptools.find_packages(exclude=["dist", "build", "*.egg-info"]),
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
     extras_require={"dev": requirements_dev},
-    entry_points={"console_scripts": ["gid = gitir_downloader.main:start",]},
+    entry_points={"console_scripts": ["gid = gitir_downloader.main:start"]},
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.8",
